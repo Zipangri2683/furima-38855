29c7982a60f61fd53bebbe731a157f70830eaf91
Zipangri2683 <s.inage.asianzu@gmail.com>
2023-03-26 18:11:19 +0900

    最終課題提出前 修正1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
+  patch '/items/:id/edit', to: 'items#update'
   resources :items do
    resources :orders, only: [:index, :create]
   end
